363081b6da6ac4b0cadc14fa5236c6481bb92eac
Phillip Palanca <palancapg@mymail.vcu.edu>
2021-08-12 23:28:56 -0400

    Added photos to the cards and created the demo lesson for debit cards

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -4,8 +4,19 @@ import React from "react";
 
 export function NavBar() {
     
-    function closePopup(){
-        document.getElementById("lesson-Details").classList.toggle("active")
+    function closePopup(num){
+        if(num == 1){
+            document.getElementById("lesson-Details1").classList.toggle("active")
+        }
+        else if(num == 2){
+            document.getElementById("lesson-Details2").classList.toggle("active")
+        }
+        else if(num == 3){
+            document.getElementById("lesson-Details3").classList.toggle("active")
+        }
+        else{
+            document.getElementById("lesson-Details4").classList.toggle("active")
+        }
         document.getElementById("transparent-overlay").classList.toggle("active")
     }
 
@@ -33,22 +44,101 @@ export function NavBar() {
                     </ul>
                 </div>
             </nav>
-            
-            <div className="center card" onClick={() => closePopup()}> 
-                <div class="container">
-                    <h4><b>What is a debit card?</b></h4> 
-                </div>
-            </div>
-            
-            <div className="lessonDetails">
-                    <div className="popUp" id="lesson-Details" >
-                        <button data-close-button className="close-btn" onClick={() => closePopup()}>&times;</button>   
-                        <h1>What is a debit card?</h1>
-                        <t1>A debit card is associated with a checking account and they let you directly access and use the money currently in your account (unlike a credit card)
-</t1>
-                    </div>
-                    <div className="overlay" id="transparent-overlay"></div>
-            </div>
+            <table className="center table">
+                <tbody>
+
+                    <tr>
+                        <div className="card" onClick={() => closePopup(1)}> 
+                            <div class="container">
+                                <h4><b>What is a debit card?</b></h4> 
+                            </div>
+                        </div>
+                
+                        <div className="lessonDetails">
+                                <div className="popUp" id="lesson-Details1" >
+                                    <button data-close-button className="close-btn" onClick={() => closePopup(1)}>&times;</button>   
+                                    <h1>What is a debit card?</h1>
+                                    <t1>A debit card is associated with a checking account and they let you directly access and use the money currently in your account (unlike a credit card)
+                                    </t1>
+                                    <img className="image" src="images/capital-one.jpg" alt="Picture of Debit Card"></img>
+                                </div>
+                                <div className="overlay" id="transparent-overlay"></div>
+                        </div>
+                    </tr>
+
+                    <br></br>
+                    <br></br>
+
+                    <tr>
+                        <div className="card" onClick={() => closePopup(2)}> 
+                            <div class="container">
+                                <h4><b>How to use a debit card?</b></h4> 
+                            </div>
+                        </div>
+                
+                        <div className="lessonDetails">
+                                <div className="popUp" id="lesson-Details2" >
+                                    <button data-close-button className="close-btn" onClick={() => closePopup(2)}>&times;</button>   
+                                    <h1>How to use a debit card?</h1>
+                                    <t1>You can use the card to make purchases directly, or you can withdraw money from an ATM using the card and a PIN number that you create when you get the card
+                                        Since a debit card uses money you currently have in your account, make sure you have enough money before making a purchase
+                                    </t1>
+                                    <img className="image" src="images/atm.png" alt="Picture of Women Using ATM"></img>
+                                </div>
+
+                                <div className="overlay" id="transparent-overlay"></div>
+                        </div>
+                    </tr>
+
+                    <br></br>
+                    <br></br>
+
+                    <tr>
+                        <div className="card" onClick={() => closePopup(4)}> 
+                            <div class="container">
+                                <h4><b>How to check debit card history?</b></h4> 
+                            </div>
+                        </div>
+                
+                        <div className="lessonDetails">
+                                <div className="popUp" id="lesson-Details4" >
+                                    <button data-close-button className="close-btn" onClick={() => closePopup(4)}>&times;</button>   
+                                    <h1>How to check debit card history?</h1>
+                                    <t1>You typically get a statement every month telling you how much, where, and when you spent money
+                                    </t1>
+                                    <img className="image" src="images/months.jpg" alt="Picture of Calendar"></img>
+                                </div>
+                                
+                                <div className="overlay" id="transparent-overlay"></div>
+                        </div>
+                    </tr>
+                    
+                    <br></br>
+                    <br></br>
+
+                    <tr>
+                        <div className="card" onClick={() => closePopup(3)}> 
+                            <div class="container">
+                                <h4><b>Why choose a debit card?</b></h4> 
+                            </div>
+                        </div>
+                
+                        <div className="lessonDetails">
+                                <div className="popUp" id="lesson-Details3" >
+                                    <button data-close-button className="close-btn" onClick={() => closePopup(3)}>&times;</button>   
+                                    <h1>Why choose a debit card?</h1>
+                                    <t1>Since you’re not borrowing anything, there is no interest charge
+Many times there is no fee for losing a card, and make sure to lock your card and report it if you do
+It helps teach budgeting skills because you can only spend money you currently have (unlike a credit card)
+                                    </t1>
+                                    <img className="image" src="images/happy kids.jpg" alt="Picture of Calendar"></img>
+                                </div>
+                                <div className="overlay" id="transparent-overlay"></div>
+                        </div>
+                    </tr>
+
+                </tbody>
+            </table>
         </div>
 
 
