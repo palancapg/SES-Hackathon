5a8c142fc94e9c936a7d993b12a0f68e827e9b87
Sumaiya Ahmed <sumaiyahmed2848@gmail.com>
2021-08-12 21:25:04 -0400

    Formatting navbar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,38 +1,36 @@
-import {Link, NavLink} from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import React from "react";
 
 
-export function NavBar()
-{
-  
-  return (
-    
-    <nav>
-    <div class="navDiv">
-      <ul>
-          <li>
-              <NavLink push to="/"><img src="images/logo.png"class="image1"/></NavLink>
-          </li>
-          
-          <li>
-              <NavLink class='nav-link nav-link2'push to="/">Beginner</NavLink>
-          </li>
-          <li>
-              <NavLink class ='nav-link'push to="/">Intermediate</NavLink>
-          </li>
-          <li>
-              <NavLink class='nav-link nav-link3' push to="/">Expert</NavLink>
-          </li>
-          <li>
-              <NavLink  push to="/"><img class="image2"src="images/plant.png"/></NavLink>
-          </li>
-          
-          
-          
-      </ul>
-    </div>
-</nav>
+export function NavBar() {
 
-);
+    return (
+
+        <nav>
+            <div class="navDiv">
+                <ul>
+                    <li class='Captial'>
+                        <NavLink push to="/"><img src="images/logo.png" class="image1" /></NavLink>
+                    </li>
+
+                    <li class='nav1'>
+                        <NavLink class='nav-link nav-link2' push to="/">Beginner</NavLink>
+                    </li>
+                    <li class='nav1'>
+                        <NavLink class='nav-link' push to="/">Intermediate</NavLink>
+                    </li>
+                    <li class='nav1'>
+                        <NavLink class='nav-link nav-link3' push to="/">Expert</NavLink>
+                    </li>
+                    <li class='tree'>
+                        <NavLink push to="/"><img class="image2" src="images/plant.png" /></NavLink>
+                    </li>
+
+
+
+                </ul>
+            </div>
+        </nav>
+
+    );
 }
-
